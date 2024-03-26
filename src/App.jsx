571cbe34315c927e14f5c68850e7d416ba48eec4
Faaiz Amir <faaizammir@gmail.com>
2024-03-27 02:02:22 +0500

    integrated authentication

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,32 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/Home/Home'
 import Login from './components/Login/Login'
 import Signup from './components/Signup/Signup'
+import { useEffect, useState } from 'react'
+import {auth} from './firebase'
 
-// 21:00 timestamp
 function App() {
-
+  const [userName, setUserName] = useState('')  
+  useEffect(() => {
+    auth.onAuthStateChanged(user => {
+      console.log(user)
+      if(user){
+        setUserName(user.displayName)
+      }
+      else{
+        setUserName('')
+      }
+    })
+  
+    
+  }, [])
+  
   return (
     <>
       <Router>
         <Routes>
           <Route 
             path='/'
-            element={<Home/>}
+            element={<Home name={userName}/>}
           />
           <Route 
             path='/login'
